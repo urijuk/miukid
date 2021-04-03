388456f8d7b188fe8f8fd61d91ccd764f21f2b32
urijuk <gelte@mail.ru>
2021-04-04 01:46:20 +0600

    first commit

---
+++ script/add_to_bin.js
@@ -0,0 +1,9 @@
+let title_product=document.getElementsByClassName("title_product");
+console.log(title_product[0].innerText);
+
+let price_product=document.getElementsByClassName("price_product");
+let price_product_del=document.getElementsByClassName("price_product_del");
+
+console.log(price_product[0].innerText);
+console.log(price_product_del[0].innerText);
+
