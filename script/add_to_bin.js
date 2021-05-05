179bc8231e38b3f3580cb3be4842e95e6d143e36
urijuk <gelte@mail.ru>
2021-05-06 00:02:54 +0600

    lesson 31

--- script/add_to_bin.js
+++ script/add_to_bin.js
@@ -2,25 +2,63 @@
 //==============HOMEWORK 30==============================//
 
 // Сделать функцию которая вычисляет длину окружности, радиус задает пользователь.
-function dlinaKruga (d){
+// function dlinaKruga (d){
+//     let p=3.14;
+//     let c=p*d
+//     console.log(c)
+// }
+// dlinaKruga (8)
+
+
+
+
+// // Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b
+// function min(a,b) {
+//     let aa=a-b
+//     let bb=b-a
+//     if (aa>0) {
+//         console.log(a);
+//     } else {
+//         console.log(b)
+//     }
+// }
+// min (5, 9)
+
+
+// ==============================LESSON 31//
+
+function name_function (peremenay) {
+    let number=50;
+    let result;
+    result=number+peremenay;
+    alert (result);
+}
+name_function(300);
+
+//==================homework 31==============//
+
+// Сделайте функцию которая возводить в степень числа вводимые пользователем//
+
+function stepen (peremenX) {
+    let numberX=4;
+    resultX=numberX**peremenX;
+    console.log (resultX)
+}
+stepen (3);
+
+// Сделайте функцию которая находит площадь круга, радиус вводить пользователь //
+
+function ploshad (radius) {
     let p=3.14;
-    let c=p*d
-    console.log(c)
+    let resultY=p*radius**2;
+    console.log (resultY)
 }
-dlinaKruga (8)
+ploshad (5);
 
+//  Сделайте функцию которая находит площадь прямоугольника, длину и ширину вводить пользователь.//
 
-
-
-// Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b
-function min(a,b) {
-    let aa=a-b
-    let bb=b-a
-    if (aa>0) {
-        console.log(a);
-    } else {
-        console.log(b)
-    }
+function ploshad_pramougolnika (a, b) {
+    let resultZ=a*b;
+    console.log (resultZ)
 }
-min (5, 9)
-
+ploshad_pramougolnika (6, 9);
