9f324132ef4c9f86d7514dd03515661cb54ce176
urijuk <gelte@mail.ru>
2021-05-19 00:54:13 +0600

    Lesson 34

--- script/add_to_bin.js
+++ script/add_to_bin.js
@@ -1,167 +1,72 @@
 
-//==============HOMEWORK 30==============================//
 
-// Сделать функцию которая вычисляет длину окружности, радиус задает пользователь.
-// function dlinaKruga (d){
-//     let p=3.14;
-//     let c=p*d
-//     console.log(c)
+//=======================LESSON 34========================//
+
+// let arr=[1, 2, 3, 4, 5];
+// console.log(arr);
+// console.log(arr.pop()); //последний извлекает//
+// console.log(arr.shift()); //первый извлекает//
+// console.log(arr);
+
+// arr.push(10); //добавляет в конец//
+// console.log(arr);
+
+// arr.unshift(15) //добавляет в начало//
+// console.log(arr);
+
+// delete arr[0];
+// delete arr[1];
+// console.log(arr)
+
+// arr[50] = 20;
+// console.log(arr)
+
+// let arr=[1, 2, 3, 4, 5];
+// for (let i=0; i<arr.length; i++) {
+//     console.log(arr[i])
 // }
-// dlinaKruga (8)
+
+// for (let value of arr) {
+//     console.log(value)
+// }  
+
+// arr.forEach(function(item, index, arr){
+//     console.log("идекс: " +index+ " значение: " +item+ " из массива: " +arr)
+// });
+
+// let object = {0:1, 1:2, 2:3, lenth:3};
+// console.log(object);
 
 
 
-
-// // Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b
-// function min(a,b) {
-//     let aa=a-b
-//     let bb=b-a
-//     if (aa>0) {
-//         console.log(a);
-//     } else {
-//         console.log(b)
-//     }
+// let array=[];
+// for (let i=0; i<object.lenth; i++) {
+//     array.push(object[i])
 // }
-// min (5, 9)
+// console.log(array)
+
+// ================HOMEWORK 34==========================//
+
+let object = {0: 'first', 1: 'second', 2: 'third', length: 3} ;
+console.log(object);
 
 
-// ==============================LESSON 31//
 
-// function name_function (peremenay) {
-//     let number=50;
-//     let result;
-//     result=number+peremenay;
-//     alert (result);
-// }
-// name_function(300);
+let array=[];
+for (let i=0; i<object.length; i++) {
+    array.push(object[i]);
+}
+console.log(array)
 
-// //==================homework 31==============//
+let massiv = ["first", "second", "third" ];
 
-// // Сделайте функцию которая возводить в степень числа вводимые пользователем//
+let vyvod1=[massiv[0]];
+let vyvod2=[massiv[1]];
+let vyvod3=[massiv[2]];
 
-// function stepen (peremenX) {
-//     let numberX=4;
-//     resultX=numberX**peremenX;
-//     console.log (resultX)
-// }
-// stepen (3);
+let vyvod=[];
+vyvod.push(vyvod1)
+vyvod.push(vyvod2)
+vyvod.push(vyvod3)
 
-// // Сделайте функцию которая находит площадь круга, радиус вводить пользователь //
-
-// function ploshad (radius) {
-//     let p=3.14;
-//     let resultY=p*radius**2;
-//     console.log (resultY)
-// }
-// ploshad (5);
-
-// //  Сделайте функцию которая находит площадь прямоугольника, длину и ширину вводить пользователь.//
-
-// function ploshad_pramougolnika (a, b) {
-//     let resultZ=a*b;
-//     console.log (resultZ)
-// }
-// ploshad_pramougolnika (6, 9);
-
-//============================LESSON 32=====================//
-// методы и свойства//
-
-// let text="Привет, как у тебя дела?";
-// console.log(text.length);
-// console.log(text.indexOf("тебя"));
-// console.log(text.search);
-// console.log(text.slice(7, 11));
-// console.log(text.replace("тебя", "меня"));
-// console.log(text.toUpperCase())
-
-// let x=123;
-// x.toString();
-// console.log(x*5)
-
-// let z=9.51524;
-// console.log(z.toExponential(2));
-
-// let y=6.48792;
-// console.log(y.toFixed(0))
-// console.log(y.toFixed(2))
-
-// let a=9;
-// let b="10";
-// let result=a+ +b;
-// console.log(result)
-
-//================HOMEWORK 32==========================//
-// сделайте функцию которая суммируем два числа с плавающей точкой
-// пример: если пользователь  ввел 1.2, 1.2 вывод должен быть 2. 
-// пример: если пользователь  ввел 2.5, 2.3 вывод должен быть 5
-// пример: если пользователь  ввел 2,5, 2.7 вывод должен быть 5. 
-// пример: если пользователь  ввел 2,5, 3,2 вывод должен быть 6.
-
-
-// let a=1.2;
-// let b=1.2;
-// let result=a+b;
-// console.log(result.toFixed(0));
-
-// let c=2.5;
-// let d=2.3;
-// let result2=c+d;
-// console.log(result2.toFixed(0));
-
-// let e=2.5;
-// let f=2.7;
-// let result3=e+f;
-// console.log(result3.toFixed(0));
-
-// let x=2.5;
-// let y=3.2;
-// let result4=x+y;
-// console.log(result4.toFixed(0))
-
-//==========LESSON 33==================//
-
-let add_to_bin = document.getElementsByClassName("addBin");
-    console.log(add_to_bin);
-
-let myFunction=function(evt){
-    evt.preventDefault();
-    
-    let product = this.parentNode.parentNode.querySelectorAll(".title_product");
-    let price_product = this.parentNode.parentNode.querySelectorAll(".price_product");
-    let img_product = this.parentNode.parentNode.querySelectorAll(".img_product img");
-
-    let creat_li = document.createElement('li');
-
-
-    console.log(product[0].innerText);
-    console.log(price_product[0].innerText);
-    console.log(img_product[0].src);
-
-   
-        
-
-    creat_li.innerHTML = '<div class="bin">\
-                                <div class="bin_img">\
-                                <img src="'+img_product[0].src+'" alt="">\
-                                </div>\
-                                <div class="bin_texts">\
-                                    <p class="bin_text_1">\
-                                        <a href="#">'+product[0].innerText+'</a>\
-                                    </p>\
-                                    <div class="">\
-                                        <span class="bin_text_bold">'+price_product[0].innerText+'</span>\
-                                        <span class="bin_text_underline">$42.00</span>\
-                                    </div>\
-                                    <div class="bin_icon">\
-                                            <i class="fab fa-bitbucket"></i>\
-                                    </div>\
-                                </div>\
-                            </div>';
-    let block_cart = document.querySelector('.block_Bin ul');
-    block_cart.appendChild(creat_li);
-
-};
-// evt.preventDefault(); не будет скорлит к началу страницы//
-
-for (var i=0;i<add_to_bin.length;i++) { 
-    add_to_bin[i].addEventListener('click', myFunction, false)}
+console.log(vyvod)
