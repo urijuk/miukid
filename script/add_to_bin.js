c2d5eb9da56607c5b7d59afa4b45f7fa1079b5dd
urijuk <gelte@mail.ru>
2021-06-12 01:41:49 +0600

    lesson 40

--- script/add_to_bin.js
+++ script/add_to_bin.js
@@ -1,21 +1,20 @@
 
 
-let add_to_bin = document.getElementsByClassName("addBin");
-    console.log(add_to_bin);
+let add_to_bin = document.querySelectorAll(".addBin");
 
 let myFunction=function(evt){
     evt.preventDefault();
     
     let product = this.parentNode.parentNode.querySelectorAll(".title_product");
-    let price_product = this.parentNode.parentNode.querySelectorAll(".price_product");
+    let price_product = this.parentNode.parentNode.querySelectorAll(".oprice");
     let img_product = this.parentNode.parentNode.querySelectorAll(".img_product img");
 
     let creat_li = document.createElement('li');
 
 
-    console.log(product[0].innerText);
-    console.log(price_product[0].innerText);
-    console.log(img_product[0].src);
+    // console.log(product[0].innerText);
+    // console.log(price_product[0].innerText);
+    // console.log(img_product[0].src);
 
    
         
@@ -29,11 +28,12 @@ let myFunction=function(evt){
                                         <a href="#">'+product[0].innerText+'</a>\
                                     </p>\
                                     <div class="">\
-                                        <span class="bin_text_bold">'+price_product[0].innerText+'</span>\
+                                        <span class="currency">$</span>\
+                                        <span class="oprice">'+price_product[0].innerText+'</span>\
                                         <span class="bin_text_underline">$42.00</span>\
                                     </div>\
-                                    <div class="bin_icon">\
-                                        <a href="#"><i class="fab fa-bitbucket"></i></a>\
+                                    <div class="bin_icon1">\
+                                        <p ><i class="fab fa-bitbucket"></i></p>\
                                     </div>\
                                 </div>\
                             </div>';
@@ -41,26 +41,40 @@ let myFunction=function(evt){
     block_cart.appendChild(creat_li);
     
     // удаление товара с корзины    //
-    let click_trash = document.querySelectorAll("bin_icon");
-
-    for (var i=0; i<click_trash.length; i++) { 
-        class_trash[i].addEventListener('click', fun_delete, false);
-    }
     
+    const btns = document.querySelectorAll('.bin_icon1')
+    // console.log(btns)
+    
+    if(btns) {
+        btns.forEach(function(el) {
+            el.addEventListener('click', () => fun_delete(el))
+        })
+    }
     function fun_delete(evt) {
-        evt.preventDefault();
-        alert("delete");
+        // console.log(this)
+        // alert("delete");
+        total_price()
 
-       this.parentNode.parentNode.parentNode.remove();
+       evt.parentNode.parentNode.remove();
     };  
-
+    total_price()
 };
 // evt.preventDefault(); не будет скорлит к началу страницы//
 
 for (var i=0;i<add_to_bin.length;i++) { 
-    add_to_bin[i].addEventListener('click', myFunction, false)}
+    add_to_bin[i].addEventListener('click', myFunction, false)
+};
 
+function total_price() {
+    let count_price = document.querySelectorAll(".oprice");
 
+    let total_price = 0;
 
-
-
+    for (i=0; i<count_price.length; i++) {
+        total_price = total_price + (+count_price[i].textContent);
+     
+    }
+    
+    document.getElementsByClassName("span_cart span2")[0].innerHTML = `$ ${total_price}`;
+    document.querySelectorAll(".total_text .total_price")[0].innerHTML = `$ ${total_price}`;
+}
