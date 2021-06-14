80ff5723d25a11e20b7b12a4187af8aa2121619b
urijuk <gelte@mail.ru>
2021-06-15 00:38:32 +0600

    lesson 43

--- script/script.js
+++ script/script.js
@@ -1,10 +1,7 @@
+//+++++++++++++++++++++++ LESSON 42 +++++++++++++++++++++++++++//
 let click  = document.getElementById("btn1");
 let click2 = document.getElementById("btn2");
 let click3 = document.getElementById("btn3");
-
-// click.onclick = function() {
-//     alert("click")
-// }
 
 let delet_btn = (e) => {
     e.target.remove();
@@ -13,3 +10,32 @@ let delet_btn = (e) => {
 click.addEventListener('click', delet_btn );
 click2.addEventListener('click', delet_btn );
 click3.addEventListener('click', delet_btn );  
+
+//+++++++++++++++++++++++ LESSON 43 +++++++++++++++++++++++++++//
+
+
+
+let btn_green = document.getElementById("bg_green");
+let btn_red = document.getElementById("bg_red");
+let btn_yellow = document.getElementById("bg_yellow");
+
+//=============green============//
+let green_btn = (e) => {
+    document.body.style.background = "green";
+    };
+
+btn_green.addEventListener('click', green_btn );
+
+//=============red============//
+let red_btn = (e) => {
+    document.body.style.background = "red";
+    };
+
+btn_red.addEventListener('click', red_btn ); 
+
+//=============yellow============//
+let yellow_btn = (e) => {
+    document.body.style.background = "yellow";
+    };
+
+btn_yellow.addEventListener('click', yellow_btn );  
