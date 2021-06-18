512baa6ee2291b87b265cc869813ef3544645469
urijuk <gelte@mail.ru>
2021-06-19 00:32:12 +0600

    lesson 45

--- script/script.js
+++ script/script.js
@@ -1,116 +1,81 @@
-// //+++++++++++++++++++++++ LESSON 42 +++++++++++++++++++++++++++//
-// let click  = document.getElementById("btn1");
-// let click2 = document.getElementById("btn2");
-// let click3 = document.getElementById("btn3");
+//========================LESSON 45=======================//
 
-// let delet_btn = (e) => {
-//     e.target.remove();
-// }
+// class ploshad {
 
-// click.addEventListener('click', delet_btn );
-// click2.addEventListener('click', delet_btn );
-// click3.addEventListener('click', delet_btn );  
-
-//+++++++++++++++++++++++ LESSON 43 +++++++++++++++++++++++++++//
-
-
-
-// let btn_green = document.getElementById("bg_green");
-// let btn_red = document.getElementById("bg_red");
-// let btn_yellow = document.getElementById("bg_yellow");
-
-// //=============green============//
-// let green_btn = (e) => {
-//     document.body.style.background = "green";
-//     };
-
-// btn_green.addEventListener('click', green_btn );
-
-// //=============red============//
-// let red_btn = (e) => {
-//     document.body.style.background = "red";
-//     };
-
-// btn_red.addEventListener('click', red_btn ); 
-
-// //=============yellow============//
-// let yellow_btn = (e) => {
-//     document.body.style.background = "yellow";
-//     };
-
-// btn_yellow.addEventListener('click', yellow_btn );  
-
-
-//++++++++++++++LESSON 44++++++++++++++++++++++++++++++++++++++//
-
-// function get_this (a, b) {
-
-//     function arif() {
-//         return a+b;
-//     };
-//     console.log(arif());
-// }
-// get_this(10, 3)
-
-// const obj = {
-//     name: "Urmat",
-//     age: 41,
-//     sum: function() {
-//         console.log(this);
+//     constructor (width, height) {
+//         this.width = width;
+//         this.height = height;
 //     }
-// };
-//  obj.sum();
 
-//  function User(name, age){
-//      this.name = name,
-//      this.age = age,
-//      this.data = function() {
-//          console.log( "Hello" + this.name);
-//     };
-// };
-// let men = new User ("Urmat", 41);
-// men.data();
+//     calcPloshad() {
+//         return this.width * this.height;
+//     }
 
-// function hello(name) {
-//     console.log(this);
-//     console.log(this.name);
-// };
+//     calcPloshad2() {
+//         return this.width + this.height;
+//     }
+// }
 
-// const newObj = {
-//     name: "urmat",
-// };
-// hello.call(newObj);
-// hello.apply(newObj);
+// const res_area = new ploshad(10, 20);
+// const res_area2 = new ploshad(30, 40);
+// console.log(res_area.calcPloshad());
+// console.log(res_area2.calcPloshad2());
 
-// const click = document.querySelector("#btn1");
+// class ploshadText extends ploshad {
+//     // extends связка двух классов
+//     constructor (width, height, text, value) {
+//         super(width, height);
+//         this.text = text;
+//         this.value = value;
+//     }
 
-// click.addEventListener('click', function() {
-//     this.style.background = "red";
-// })
+//     showText() {
+//         console.log(`Text: ${this.text} | Value: ${this.value}`);
+//     }
+// }
+//  const block = new ploshadText (20, 20, "Urmat", "Тема урока класс");
 
+//  block.showText();
+//  console.log(block.calcPloshad());
 
+ //===================HOMEWORK 45==================//
 
+//  Реализуйте класс Student (Студент), который будет наследовать от класса User,
+//  подобно тому, как это сделано в теоретической части урока. Этот класс
+//  должен иметь следующие свойства: name (имя, наследуется от User),
+//  surname (фамилия, наследуется от User), year (год поступления в вуз).
+//  Класс должен иметь метод getFullName() (наследуется от User),
+//  с помощью которого можно вывести одновременно имя и фамилию студента.
+//  Также класс должен иметь метод getCourse(), который будет выводить 
+//  текущий курс студента (от 1 до 5). Курс вычисляется
+//  так: нужно от текущего года отнять год поступления в вуз.
+//  Текущий год получите самостоятельно.
 
+class user {
 
+    constructor (name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
-//=======================HOMEwork 44=============================//
-// Сделать объект, который запрашивает имя пользователя, год рождения, место рождения, с этим объектом должны создаваться все пользователи;
-// данные о пользователе вывести на страницу 
+    getFullName() {
+        return this.name + this.surname;
+    }
+}
 
-// input 
-// let amantur= new createUser();
-// output: 
-// Amantur, 1998, Bishkek.
+class student extends user {
+   
+    constructor (name, surname, year, this_year) {
+        super(name, surname);
+        this.year = year;
+        this.this_year = this_year;
+    }
 
+    getCourse() {
+        return this.this_year-this.year
+    }
+}
+ const i_am = new student ("Urmat ", "Zhunushov", 2020, 2021);
 
-
- function User(name, age, city){
-     this.name = name,
-     this.age = age,
-     this.city = city,
-     this.data = function() {
-         console.log(this.name, this.age, this.city);
-    };
-};
-let men = new User ("Urmat", 41, "Bishkek");
-men.data();
+ i_am.getCourse();
+ console.log(`I ${i_am.getFullName()}`, `learning in course ${i_am.getCourse()}`);
