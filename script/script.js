9687eaaee4d507292a3b099b3fdc38590075fd4e
urijuk <gelte@mail.ru>
2021-07-07 13:20:56 +0600

    lesson52

--- script/script.js
+++ script/script.js
@@ -21,24 +21,72 @@
 (function () {
 'use strict';
 
-const inputSom = document.querySelector("#som");
-const inputUsd = document.querySelector("#usd");
+// const inputSom = document.querySelector("#som");
+// const inputUsd = document.querySelector("#usd");
 
-inputSom.addEventListener( 'input', ()=> {
-    const request = new XMLHttpRequest();
-    request.open("GET", "current.json");
-    request.setRequestHeader("content", "application/json; sharset=UTF-8");
-    request.send();
+// inputSom.addEventListener( 'input', ()=> {
+//     const request = new XMLHttpRequest();
+//     request.open("GET", "current.json");
+//     request.setRequestHeader("content", "application/json; sharset=UTF-8");
+//     request.send();
 
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState ===4 && request.status ===200) {
-            const currency = JSON.parse(request.response);
-            const result =  inputSom.value/currency.current.usd;
-            inputUsd.value = (result).toFixed(2);
-        }
-    })
-} );
+//     request.addEventListener('readystatechange', () => {
+//         if(request.readyState ===4 && request.status ===200) {
+//             const currency = JSON.parse(request.response);
+//             const result =  inputSom.value/currency.current.usd;
+//             inputUsd.value = (result).toFixed(2);
+//         }
+//     })
+// } );
 
+//====================LESSON 52========================//
+
+
+//filter
+
+// const array = ["Яблоки", "Груши", "Виноград", "Апельсин"];
+// const result = array.filter(function(fruit) {
+
+//     if (fruit.length < 7) {
+//         return fruit
+//     }
+// });
+
+// console.log(result);
+
+//map
+
+// const array = ["Яблоки", "ГРУШИ", "Виноград", "Апельсин"];
+// const result = array.map(item=>item.toLowerCase());
+
+// console.log(result);
+
+//every/some
+
+// const array = [5, "ГРУШИ", "Виноград", "Апельсин"];
+
+// const result = array.some(item=>typeof(item)=='number');
+// const result = array.every(item=>typeof(item)=='number');
+
+// console.log(result);
+
+// reduce
+
+// const array = [1,2,3,4,5];
+
+// const result = array.reduce((count, item) => count+item);
+// console.log(result);
+
+const obj = {
+    dzunushov: "sname",
+    urmat: "name",
+    42: "age"
+};
+
+
+const result = Object.entries(obj)
+.filter(item => console.log(item[0]));
+console.log(result)
 
 
 }());
