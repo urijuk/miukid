4e58d56430e5dd7a0aa2c74d3f78fd342c98d9a6
urijuk <gelte@mail.ru>
2021-07-12 00:21:33 +0600

    Lesson 59

--- script/script.js
+++ script/script.js
@@ -27,29 +27,63 @@
 // все эти пункты сделать с геттерами и сеттерами. 
 
 
-let names = prompt("Введите имя: ");
-let surnames = prompt("Введите фамилию: ");
+// let names = prompt("Введите имя: ");
+// let surnames = prompt("Введите фамилию: ");
 
-const person = {
-    names: names,
-    surnames: surnames,
+// const person = {
+//     names: names,
+//     surnames: surnames,
 
-    get userName() {
-        return this.surnames;
-    },
+//     get userName() {
+//         return this.surnames;
+//     },
     
-    set userName(num) {
-        this.surnames = num;
-    }
-};
+//     set userName(num) {
+//         this.surnames = num;
+//     }
+// };
 
-const lengthNames = names.length + surnames.length;
+// const lengthNames = names.length + surnames.length;
 
 
-if(lengthNames > 6) {
-    console.log(lengthNames);
+// if(lengthNames > 6) {
+//     console.log(lengthNames);
+// }
+// else {
+//     console.log("имя и фамилие минимально должны быть 6 символов")
+// }
+
+
+//=====================HOMEEORK 59======//
+
+function User(name, age) {
+    this.name = name;
+
+    let userAge = age;
+
+    this.get = function() {
+        console.log(`Имя пользователя: ${this.name}, Возраст: ${this.age}`);
+    };
+
+    this.getAge = function() {
+        return userAge;
+    };
+
+    this.setAge = function(age) {
+
+        if(typeof age == "number" && age>1 && age<150) {
+            userAge = age;
+        } else {
+            console.log("Недопустимое значение!");
+        }
+    };
 }
-else {
-    console.log("имя и фамилие минимально должны быть 6 символов")
-}
 
+const userData = new User("Urmat", 42);
+
+console.log(userData.name);
+console.log(userData.getAge());
+
+userData.setAge(90);
+
+console.log(userData.getAge());
